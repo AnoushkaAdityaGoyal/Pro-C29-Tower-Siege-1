7cf891d9aeb17489f4cb2e70c9f4eab19e6e9c59
AnoushkaAdityaGoyal <74051619+AnoushkaAdityaGoyal@users.noreply.github.com>
2021-01-05 15:15:06 +0530

    Add files via upload

---
+++ Box4.js
@@ -0,0 +1,20 @@
+class Box4 {
+    constructor(x, y, width, height) {
+      var options = {
+        'isStatic': false,
+        'restitution':0.8,
+        'friction':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display() {
+      push();
+      rectMode(CENTER);
+      fill(144, 237, 144)
+      rect(this.body.position.x, this.body.position.y, this.width, this.height);
+      pop();
+    }
+  }
